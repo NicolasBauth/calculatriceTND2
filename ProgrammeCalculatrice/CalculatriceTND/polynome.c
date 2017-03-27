31a5cd3ac886739b2edeffca5ae206836ae371fd
NicolasBauth <nicolas.bauthier.01@student.henallux.be>
2017-03-27 18:20:52 +0200

    Maj clean code x0-->x

--- ProgrammeCalculatrice/CalculatriceTND/polynome.c
+++ ProgrammeCalculatrice/CalculatriceTND/polynome.c
@@ -5,10 +5,11 @@
 
 
 double newtonRaphson(double x0, double tabCoefficients[], int tailleTableau) {
+	double x = x0;
 	for (int i = 0; i < 20; i++) {
-		x0 -= f(x0, tabCoefficients, tailleTableau) / fprime(x0, tabCoefficients, tailleTableau);
+		x -= f(x, tabCoefficients, tailleTableau) / fprime(x, tabCoefficients, tailleTableau);
 	}
-	return x0;
+	return x;
 }
 
 double f(double x, double tabCoefficients[], int tailleTableau) {
